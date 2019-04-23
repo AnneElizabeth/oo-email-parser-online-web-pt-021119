5020f73721aad34c3c96b543c5f0bbdfa0d31399
Anne Edmondson <edmondson.anne@gmail.com>
2019-04-23 04:17:16 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,6 +4,14 @@
 # or whitespace (' ').
 
 
-def EmailParser
-  
+class EmailParser
+  attr_accessor :emails
+
+  def initialize(emails)
+    @emails = emails
+  end
+
+  def parse
+    self.emails.split(", ").map{|e| e.split(" ")}.flatten.uniq
+  end
 end
